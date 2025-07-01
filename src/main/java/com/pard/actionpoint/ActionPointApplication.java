afdff3691f169285a13a3a8b91df1c092d94a88a
SanggeunCho <wtr011124@gmail.com>
2025-07-01 17:07:45 +0900

    Initial Commit

---
+++ src/main/java/com/pard/actionpoint/ActionPointApplication.java
@@ -0,0 +1,13 @@
+package com.pard.actionpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pard/actionpoint/ActionPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.actionpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
