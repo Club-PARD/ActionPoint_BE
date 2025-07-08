f0ce06ea53def224a0bc8af2d4d593e712ce39be
SanggeunCho <wtr011124@gmail.com>
2025-07-08 15:52:29 +0900

    edit CorsConfig

--- src/main/java/com/pard/actionpoint/common/config/CorsConfig.java
+++ src/main/java/com/pard/actionpoint/common/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 
 public class CorsConfig {
@@ -17,7 +19,7 @@ public class CorsConfig {
 
         config.setAllowCredentials(true);
 
-        config.addAllowedOrigin("*");
+        config.setAllowedOrigins(Arrays.asList("thttp://localhost:3000"));
 
         config.addAllowedHeader("*");
 
