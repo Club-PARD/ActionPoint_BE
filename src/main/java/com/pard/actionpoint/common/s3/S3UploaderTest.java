c3419b6013241b5d486e86578cbe95b7ac7afada
SanggeunCho <wtr011124@gmail.com>
2025-07-09 13:50:50 +0900

    edit S3UploaderTest

--- src/main/java/com/pard/actionpoint/common/s3/S3UploaderTest.java
+++ src/main/java/com/pard/actionpoint/common/s3/S3UploaderTest.java
@@ -1,9 +1,19 @@
 package com.pard.actionpoint.common.s3;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Profile("test")
 @Component
-public class S3UploaderTest {
+public class S3UploaderTest extends S3Uploader {
+    public S3UploaderTest(AmazonS3 amazonS3) {
+        super(amazonS3);
+    }
+
+    @Override
+    public String upload(MultipartFile multipartFile, String dirName){
+        return "https://fake-url.com/test.jpg";
+    }
 }
